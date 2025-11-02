98164e5c5c12c8f5b7767b1cb1455999104bb492
deepder <15874752+deepder@user.noreply.gitee.com>
2025-11-02 10:13:31 +0800

    1. 更新sing-box3模版

--- sing-box.sh
+++ sing-box.sh
@@ -17,7 +17,8 @@ PROTOCOL_LIST=("XTLS + reality" "hysteria2" "tuic" "ShadowTLS" "shadowsocks" "tr
 NODE_TAG=("xtls-reality" "hysteria2" "tuic" "ShadowTLS" "shadowsocks" "trojan" "vmess-ws" "vless-ws-tls" "h2-reality" "grpc-reality" "anytls")
 CONSECUTIVE_PORTS=${#PROTOCOL_LIST[@]}
 CDN_DOMAIN=("skk.moe" "ip.sb" "time.is" "cfip.xxxxxxxx.tk" "bestcf.top" "cdn.2020111.xyz" "xn--b6gac.eu.org")
-SUBSCRIBE_TEMPLATE="https://raw.githubusercontent.com/gdsunwtbxlahpwmqfs/client_template/main"
+#SUBSCRIBE_TEMPLATE="https://raw.githubusercontent.com/gdsunwtbxlahpwmqfs/client_template/main"
+SUBSCRIBE_TEMPLATE="https://raw.githubusercontent.com/gdsunwtbxlahpwmqfs/client_template/feature/hello"
 DEFAULT_NEWEST_VERSION='1.12.0-beta.15'
 
 export DEBIAN_FRONTEND=noninteractive
